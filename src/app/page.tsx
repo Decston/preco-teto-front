912b6e58aab767cd108ea59d01de083d8160ba8b
Alisson Patricio Gomes de Freitas <alissonpatriciodefreitas@gmail.com>
2025-10-29 16:27:20 -0300

    Adicionada a biblioteca shadcn e alterado a pagina principal do projeto.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator"
+
 const Home = () => {
   return (
-    <div>Preço Teto de Ações</div>
+    <div className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg">
+        <CardHeader>
+          <CardTitle>Calcular Preço Teto de Ações</CardTitle>
+          <CardDescription>Calculadora de Preço Teto de Ações</CardDescription>
+        </CardHeader>
+        <Separator />
+        <CardContent className="flex gap-2">
+          <Input placeholder="preço atual" />
+          <Button>Calcular</Button>
+        </CardContent>
+        <Separator />
+        <CardFooter>
+          <p>A. Patrício Investimentos</p>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
 
